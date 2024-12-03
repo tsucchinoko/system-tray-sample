c3c20901c66d4331210ff688c4d846fa24f28c85
tsuchiya-d <tsuchiya-d@aidemy.co.jp>
2024-12-03 18:51:02 +0900

    feat: メニューアイテムの追加

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,7 @@
-use tauri::tray::TrayIconBuilder;
+use tauri::{
+    menu::{Menu, MenuItem},
+    tray::TrayIconBuilder,
+};
 
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
@@ -10,7 +13,12 @@ fn greet(name: &str) -> String {
 pub fn run() {
     tauri::Builder::default()
         .setup(|app| {
+            // メニューの追加
+            let quit_i = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
+            let menu = Menu::with_items(app, &[&quit_i])?;
             let _tray = TrayIconBuilder::new()
+                .menu(&menu)
+                .menu_on_left_click(true)
                 .icon(app.default_window_icon().unwrap().clone())
                 .build(app)?;
             Ok(())
